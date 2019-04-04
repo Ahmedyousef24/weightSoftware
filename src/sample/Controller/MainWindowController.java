ed99f4be495eb431ce0d483efd0184ef026f39f6
moah1624 <moah1624@student.ju.se>
2019-04-04 22:49:39 +0200

    added som files

--- src/sample/Controller/MainWindowController.java
+++ src/sample/Controller/MainWindowController.java
@@ -1,4 +1,4 @@
-package sample;
+package sample.Controller;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -9,8 +9,9 @@ import javafx.stage.Stage;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class Controller implements Initializable {
+public class MainWindowController implements Initializable {
 
+    //Function for when close button is pressed.
     @FXML
     void onClosePressed(MouseEvent event) {
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
